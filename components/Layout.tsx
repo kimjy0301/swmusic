64b3b5f9042ef324f5757b0ed91b863200a99a49
KimJiYong <znzld505@naver.com>
2022-01-24 16:08:10 +0900

    asd

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,13 +3,13 @@ const Layout = (props: any) => {
     <>
       <div className="bg-slate-100 h-screen">
         <div className="w-screen flex justify-start h-16 items-center bg-slate-200 fixed top-0 z-50">
-          <div className="font-bold text-2xl hover:underline cursor-pointer mx-10">
+          <div className="font-bold text-2xl hover:underline cursor-pointer mx-10 ">
             HOME
           </div>
-          <div className="font-bold text-2xl hover:underline cursor-pointer mx-10">
+          <div className="font-bold text-2xl hover:underline cursor-pointer mx-10 ">
             CATALOG
           </div>
-          <div className="font-bold text-2xl hover:underline cursor-pointer mx-10">
+          <div className="font-bold text-2xl hover:underline cursor-pointer mx-10 ">
             CONTACT
           </div>
         </div>
