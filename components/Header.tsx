98f0c3efc468f9bca7b16341e3e69915ff765277
kimjy0301 <znzld505@naver.com>
2022-01-27 00:00:37 +0900

    header변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import logo from "../public/logo.png";
 const Header = () => {
   return (
-    <div className="w-screen flex justify-between h-16 items-center bg-gray-100 fixed top-0 z-50 min-w-min ">
-      <div className="relative w-56 flex items-center justify-center cursor-pointer ml-10 ">
+    <div className="w-screen flex md:justify-between h-16 items-center bg-gray-100 fixed top-0 z-50 min-w-min ">
+      <div className="relative md:w-56 w-28 flex items-center justify-center cursor-pointer ml-2 md:ml-10 ">
         <Link href="/">
           <a>
             <Image src={logo} alt={"swmusic logo"}></Image>
@@ -12,7 +12,7 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="flex">
+      <div className=" hidden md:flex ">
         <Link href="/">
           <a>
             <div className="font-bold text-2xl hover:underline cursor-pointer mx-10 select-none ">
@@ -30,6 +30,30 @@ const Header = () => {
         <Link href="/contact">
           <a>
             <div className="font-bold text-2xl hover:underline cursor-pointer mx-10 select-none ">
+              CONTACT
+            </div>
+          </a>
+        </Link>
+      </div>
+
+      <div className="flex md:hidden ">
+        <Link href="/">
+          <a>
+            <div className="font-bold text-sm hover:underline cursor-pointer mx-2 select-none ">
+              HOME
+            </div>
+          </a>
+        </Link>
+        <Link href="/catalog/3">
+          <a>
+            <div className="font-bold text-sm hover:underline cursor-pointer mx-2 select-none ">
+              CATALOG
+            </div>
+          </a>
+        </Link>
+        <Link href="/contact">
+          <a>
+            <div className="font-bold text-sm hover:underline cursor-pointer mx-2 select-none ">
               CONTACT
             </div>
           </a>
