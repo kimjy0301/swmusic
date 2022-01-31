2ed5d2826008a0a6f8da9eea341ce85126666161
kimjy0301 <znzld505@naver.com>
2022-02-01 00:07:54 +0900

    asdfasd

--- components/CatalogLayout.tsx
+++ components/CatalogLayout.tsx
@@ -17,7 +17,8 @@ const CatalogLayout = (props: any) => {
 
   const styles3 = useSpring({
     opacity: foldedMenu ? 0.7 : 0.8,
-    width: foldedMenu ? 23 : 250,
+    width: foldedMenu ? 250 : 250,
+    x: foldedMenu ? -260 : 0,
   });
   const transitions = useTransition(show, {
     from: { opacity: 0 },
@@ -31,39 +32,36 @@ const CatalogLayout = (props: any) => {
         (styles, item) =>
           item && (
             <animated.div style={styles}>
-              <animated.div style={{ opacity: styles3.opacity }}>
+              <animated.div>
                 <animated.div
-                  style={{ width: styles3.width }}
-                  className="absolute bg-slate-100/95 left-0 top-16 catalog-layout rounded shadow-lg text-sm lg:text-lg h-5/6 mt-2 text-center flex items-center justify-center overflow-y-auto"
+                  style={{ translateX: styles3.x }}
+                  className="absolute border-r-2 border-b-2 border-gray-300 bg-slate-100/95 left-0 top-16 catalog-layout rounded shadow-lg text-sm lg:text-lg h-5/6 mt-2 text-center flex items-center justify-center overflow-y-auto"
                 >
-                  {foldedMenu || (
-                    <>
-                      <div className="flex flex-col items-center justify-center">
-                        <Link href="/catalog/1">
-                          <a className="mt-1 mx-2 hover:font-bold">
-                            BLISTER PACKING OR VINLY PACKING
-                          </a>
-                        </Link>
-                        <Link href="/catalog/2">
-                          <a className="mt-1 mx-2 hover:font-bold">
-                            ACCESSORIES
-                          </a>
-                        </Link>
-                        <Link href="/catalog/3">
-                          <a className="mt-1 mx-2 hover:font-bold">
-                            MACHINE HEAD OR TUNING KEY
-                          </a>
-                        </Link>
-                        <Link href="/catalog/4">
-                          <a className="mt-1 mx-2 hover:font-bold">
-                            BLISTER PACKING OR VINLY PACKING
-                          </a>
-                        </Link>
-                      </div>
-                    </>
-                  )}
+                  <>
+                    <div className="flex flex-col items-center justify-center">
+                      <Link href="/catalog/1">
+                        <a className="mt-1 mx-2 hover:font-bold">
+                          BLISTER PACKING OR VINLY PACKING
+                        </a>
+                      </Link>
+                      <Link href="/catalog/2">
+                        <a className="mt-1 mx-2 hover:font-bold">ACCESSORIES</a>
+                      </Link>
+                      <Link href="/catalog/3">
+                        <a className="mt-1 mx-2 hover:font-bold">
+                          MACHINE HEAD OR TUNING KEY
+                        </a>
+                      </Link>
+                      <Link href="/catalog/4">
+                        <a className="mt-1 mx-2 hover:font-bold">
+                          BLISTER PACKING OR VINLY PACKING
+                        </a>
+                      </Link>
+                    </div>
+                  </>
+
                   <div
-                    className="right-0 h-full border-l-2 flex justify-center items-center border-white  "
+                    className="right-0 h-full border-l-2 flex justify-center items-center border-gray-200 cursor-pointer"
                     onClick={() => {
                       setFoldedMenu(!foldedMenu);
                     }}
@@ -71,7 +69,7 @@ const CatalogLayout = (props: any) => {
                     {foldedMenu ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 cursor-pointer text-gray-700"
+                        className="h-6 w-6 text-gray-700"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
@@ -86,7 +84,7 @@ const CatalogLayout = (props: any) => {
                     ) : (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 cursor-pointer text-gray-700"
+                        className="h-6 w-6 text-gray-700"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
