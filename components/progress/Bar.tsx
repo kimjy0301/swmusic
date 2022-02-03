20578bd8c3a1815e34f34e43c54ef66b9a90e150
kimjy0301 <znzld505@naver.com>
2022-02-03 14:04:45 +0900

    asdfasdfasdfasdf111

--- components/progress/Bar.tsx
+++ components/progress/Bar.tsx
@@ -1,7 +1,7 @@
 export const Bar = ({ animationDuration, progress }: any) => {
   return (
     <div
-      className="bg-indigo-600 h-2 w-full left-0 top-16 absolute z-50"
+      className="bg-indigo-600 h-2 w-full left-0 top-16 z-50 fixed"
       style={{
         marginLeft: `${(-1 + progress) * 100}%`,
         transition: `margin-left ${animationDuration}ms linear`,
