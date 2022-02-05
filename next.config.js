4d5a998715379a627f2ce05ba496f223aea1aa3b
kimjy0301 <znzld505@naver.com>
2022-02-06 00:42:10 +0900

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["146.56.147.155"],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [96, 96, 96, 96, 96, 128, 256, 384],
   },
 };
 module.exports = nextConfig;
