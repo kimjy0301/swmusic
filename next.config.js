386db5925ae3a07b664b1b4b2db9d023c0121de0
kimjy0301 <znzld505@naver.com>
2022-02-06 12:07:34 +0900

    sdfasdf

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     domains: ["146.56.147.155"],
     deviceSizes: [828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [96, 96, 96, 96, 96, 128, 256, 384],
+    imageSizes: [96, 96, 96, 128, 256, 384],
   },
 };
 module.exports = nextConfig;
