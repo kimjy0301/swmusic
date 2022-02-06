352ff5f169364c9cd604ba7442e838b0ef4b363c
kimjy0301 <znzld505@naver.com>
2022-02-06 22:38:37 +0900

    레이아웃 변경test

--- pages/catalog/[catalogid]/[id].tsx
+++ pages/catalog/[catalogid]/[id].tsx
@@ -114,6 +114,18 @@ const CatalogIndex = ({ imageProps, imageProps2, pageProps }: any) => {
 
   useEffect(() => {
     if (window.innerWidth < 1024) {
+      const catalogEle: HTMLBaseElement | null =
+        document.querySelector(".catalog");
+
+      if (catalogEle) {
+        catalogEle.style.width = "330px";
+        catalogEle.style.maxWidth = "330px";
+        catalogEle.style.minWidth = "330px";
+
+        catalogEle.style.maxHeight = "464px";
+        catalogEle.style.height = "464px";
+      }
+
       setMobile(true);
     }
   }, [mobile, setMobile]);
@@ -171,7 +183,7 @@ const CatalogIndex = ({ imageProps, imageProps2, pageProps }: any) => {
                         alt="test"
                         placeholder={"blur"}
                         quality={75}
-                        layout={"responsive"}
+                        layout={"intrinsic"}
                       />
                     </div>
                     {imageProps2.blurDataURL && (
@@ -181,7 +193,7 @@ const CatalogIndex = ({ imageProps, imageProps2, pageProps }: any) => {
                           {...imageProps2}
                           alt="test"
                           placeholder={"blur"}
-                          layout={"responsive"}
+                          layout={"intrinsic"}
                         />
                       </div>
                     )}
