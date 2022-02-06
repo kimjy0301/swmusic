c273f6603712a580bd8084f6ae6dd92aed630a60
kimjy0301 <znzld505@naver.com>
2022-02-06 14:17:14 +0900

    SDFsdfg

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["146.56.147.155"],
-    deviceSizes: [828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [96, 96, 96, 128, 256, 384],
+    deviceSizes: [1200, 1920, 2048, 3840],
+    imageSizes: [128, 128, 256, 384],
   },
 };
 module.exports = nextConfig;
