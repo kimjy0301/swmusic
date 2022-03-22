e5684520905d2a9e96c26be1acccde89ba0e6689
kimjy0301 <znzld505@naver.com>
2022-03-22 18:32:54 +0900

    sitemap test

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://swmusic.co.kr",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
